b8a9ecf557fac58644bc771455c6d9e5f13ebf2e
김상현 <llmm030506@naver.com>
2021-01-27 15:03:46 +0900

    [ADD]
    - AppConfig (관심사의 분리)

--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -11,12 +11,13 @@ import hello.core.order.OrderServiceImpl;
 public class OrderApp {
 
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl(memberRepository);
-        OrderService orderService = new OrderServiceImpl(memberRepository, discountPolicy);
+        AppConfig appConfig = new AppConfig();
+        MemberService memberservice = appConfig.memberService();
+        OrderService orderService = appConfig.orderService();
 
         Long memberId = 1L;
         Member member = new Member(memberId, "MemberA", Grade.VIP);
-        memberService.join(member);
+        memberservice.join(member);
 
         Order order = orderService.createOrder(memberId, "itmeA", 20000);
         System.out.println("order = " + order);
