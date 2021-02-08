cbe5f45872520ace3c015c92ca9aeed76657ba71
김상현 <llmm030506@naver.com>
2021-02-08 22:36:07 +0900

    [ADD] Configuration과 싱글톤
    스프링 컨테이너는 어떤 상황이와도 싱글톤을 지켜야한다

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -15,18 +15,24 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppConfig {
 
+    //@Bean -> MemberService -> new MemoryMemberRepository()
+    //@Bean -> OrderService -> new MemoryMemberRepository()
+
     @Bean
     public MemberService memberService(){
+        System.out.println("Call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
     public MemberRepository memberRepository() {
+        System.out.println("Call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
     @Bean
     public OrderService orderService(){
+        System.out.println("Call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
